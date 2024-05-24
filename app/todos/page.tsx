7d6ddc3516144c66bfc61c0cd891dca3540178ad
pksilen <petri.silen@silensoft.com>
2024-05-24 20:43:33 +0300

    Update

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,3 +1,26 @@
-export default function Todos() {
-  return <div>Todos</div>;
+'use client';
+
+import { AddTodo } from '@/app/todos/components/addtodo/AddTodo';
+import { Controls } from '@/app/todos/components/controls/Controls';
+import { ErrorBoundary } from '@/app/todos/components/errorboundary/ErrorBoundary';
+import { Header } from '@/app/todos/components/header/Header';
+import { useControlsStore } from '@/app/todos/stores/controls/controlsStore';
+import classes from './App.module.scss';
+import { TodosList } from './components/todos/TodosList';
+import { TodosTable } from './components/todos/TodosTable';
+
+export default function TodosPage() {
+  const theme = useControlsStore((store) => store.theme);
+  const viewType = useControlsStore((store) => store.viewType);
+
+  return (
+    <main className={classes.main}>
+      <Header />
+      <Controls />
+      <ErrorBoundary>
+        {viewType === 'list' ? <TodosList /> : <TodosTable />}
+        <AddTodo />
+      </ErrorBoundary>
+    </main>
+  );
 }
